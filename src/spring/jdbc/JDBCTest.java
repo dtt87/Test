bef4c4033a97a1a72b5b9b68e38035a61fcfbf01
dong_tt <dong_tt@neusoft.com>
2017-06-24 16:12:05 +0800

    springjdbc

---
+++ src/spring/jdbc/JDBCTest.java
@@ -0,0 +1,33 @@
+package spring.jdbc;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import spring.jdbc.Employee;
+
+public class JDBCTest {
+	
+	private ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext-JDBC.xml");
+	private JdbcTemplate jdbcTemplate  = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+	
+	
+	@Test
+	public void testDataSource() throws SQLException {
+		DataSource dataSource = ctx.getBean(DataSource.class);
+		System.out.println(dataSource.getConnection());
+	}
+	
+	@Test
+	public void testUpdate(){
+		String sql = "UPDATE employees SET last_name = ? WHERE id = ?";
+		jdbcTemplate.update(sql, "Bob", 1);
+	}
+}
